b83dfee05513802e0ccc4ddb713a26bb099c576e
evsworld <evanlhendrix@gmail.com>
2021-02-13 17:16:34 +0100

    imp: wire up react-query

--- src/web-client/src/App.js
+++ src/web-client/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactQueryExample } from './components/ReactQueryExample';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <ReactQueryExample />
       </header>
     </div>
   );
