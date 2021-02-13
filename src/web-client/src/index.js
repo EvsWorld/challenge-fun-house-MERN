2810999932c6a2fbb61ca46bb4ae39b8375c11b5
evsworld <evanlhendrix@gmail.com>
2021-02-13 17:10:32 +0100

    imp: set up linting on front

--- src/web-client/src/index.js
+++ src/web-client/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
