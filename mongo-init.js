41cfbbe7dd5dcb8d4582fe1a276f0fd9d9be56eb
evsworld <evanlhendrix@gmail.com>
2022-03-02 22:08:31 +0100

    imp: fwv: now seeding database with org chart base

--- mongo-init.js
+++ mongo-init.js
@@ -8,16 +8,6 @@
  * local directory directly to the container directory.
  */
 
-// Create an application user for the "SFA" database, this is the Spring Boot repository user.
-
-// import fs from "fs";
-
-// const jsonData = fs.readFileSync(
-//   __dirname + "./mongo-seed/orgPersonsAbc.json",
-//   "utf-8"
-// );
-// const personsData = JSON.parse(jsonData);
-
 const personsData = [
   {
     name: "root",
@@ -77,128 +67,13 @@ print(
   "Start #################################################################"
 );
 
-db = db.getSiblingDB("challenge-fun-house-db-t");
+db = db.getSiblingDB("challenge-fun-house-db");
 db.createUser({
   user: "api_user_t",
   pwd: "api1234t",
-  roles: [{ role: "readWrite", db: "challenge-fun-house-db-t" }],
+  roles: [{ role: "readWrite", db: "challenge-fun-house-db" }],
 });
-db.createCollection("orgPersonsT");
-db.orgPersonsT.insertMany(personsData);
-
-// ******************
-// db.db = db.getSiblingDB("api_prod_db");
-// db.createUser({
-//   user: "api_user",
-//   pwd: "api1234",
-//   roles: [{ role: "readWrite", db: "api_prod_db" }],
-// });
-// db.createCollection("uzers");
-
-// db = db.getSiblingDB("api_dev_db");
-// db.createUser({
-//   user: "api_user",
-//   pwd: "api1234",
-//   roles: [{ role: "readWrite", db: "api_dev_db" }],
-// });
-// db.createCollection("uzers");
-
-// db = db.getSiblingDB("api_test_db");
-// db.createUser({
-//   user: "api_user",
-//   pwd: "api1234",
-//   roles: [{ role: "readWrite", db: "api_test_db" }],
-// });
-// db.createCollection("uzers");
+db.createCollection("orgPersons");
+db.orgPersons.insertMany(personsData);
 
 print("END #################################################################");
-
-// console.log("ran mongoinit.js!!!!! :>> ");
-// db.createUser({
-//   user: "sfa",
-//   pwd: "veryfineindeed",
-//   roles: [
-//     {
-//       role: "readWrite",
-//       db: "challenge-fun-house-db",
-//     },
-//   ],
-// });
-
-// // Switch to the SFA database
-
-// db = db.getSiblingDB("challenge-fun-house-db");
-// db.createCollection("user");
-// db.createCollection("orgPersons");
-
-// // Create document indices
-
-// db.user.createIndex({ username: 1 });
-
-// db.profile.createIndex({ username: 1 });
-
-// // Create seed-data for the user collection
-
-// // pw: toomanysecrets
-// db.user.insert({
-//   username: "mark",
-//   password: "$2y$10$WxFrxAmdzXl21MSwydIBXeNzRCLqCeTzxPY6ow/0yq.v1ZfLl/6R6",
-//   authorities: ["GET_MY_USER", "GET_MY_PROFILE", "UPDATE_MY_PROFILE"],
-// });
-
-// // pw: nexus6
-// db.user.insert({
-//   username: "deckard",
-//   password: "$2y$10$Pyax032BmLcgazp1xYWygeCpfapylu5cUI5fRe5Xk.nmNPQ7Ae1lm",
-//   authorities: ["GET_MY_USER", "GET_MY_PROFILE", "UPDATE_MY_PROFILE"],
-// });
-
-// // pw:daisy
-// db.user.insert({
-//   username: "babayaga",
-//   password: "$2y$10$vnI90h6Y2nkJAEz33.XO1.fCbkuV84Mjqvy.vlrfxlfJ/OvmoDequ",
-//   authorities: ["GET_MY_USER", "GET_MY_PROFILE", "UPDATE_MY_PROFILE"],
-// });
-
-// // pw:iambatman
-// db.user.insert({
-//   username: "bruce",
-//   password: "$2y$10$DnsBGCMU26myi0Si1JAaL.ekIyRCi38uF1ONGwe8xB.bYvcH2EHxe",
-//   authorities: ["GET_MY_USER", "GET_MY_PROFILE", "UPDATE_MY_PROFILE"],
-// });
-
-// // Create seed-data for the profile collection
-
-// db.orgPersons.insert({
-//   username: "mark",
-//   firstName: "Mark",
-//   lastName: "Lee",
-//   position: "The Boss",
-//   aboutMe: "I am like so totes awesome.",
-// });
-
-// db.orgPersons.insert({
-//   username: "deckard",
-//   firstName: "Rick",
-//   lastName: "Deckard",
-//   position: "Blade Runner",
-//   aboutMe:
-//     "Blade Runner, retired. I've seen things you people wouldn't believe.",
-// });
-
-// db.orgPersons.insert({
-//   username: "babayaga",
-//   firstName: "John",
-//   lastName: "Wick",
-//   position: "Assassin",
-//   aboutMe: "I am revenge. And somewhat totally awesome.",
-// });
-
-// db.orgPersons.insert({
-//   username: "bruce",
-//   firstName: "Bruce",
-//   lastName: "Wayne",
-//   position: "Vigilante",
-//   aboutMe:
-//     "The world's greatest detective. Dark Knight. Eccentric billionaire.",
-// });
