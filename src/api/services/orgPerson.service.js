56734e46e0d3e020791a51f3a2d80614f68f5753
evsworld <evanlhendrix@gmail.com>
2022-03-10 16:43:40 +0100

    add console.logs

--- src/api/services/orgPerson.service.js
+++ src/api/services/orgPerson.service.js
@@ -12,6 +12,10 @@ export function makeTree(entities) {
       throw "mince! something is rotten in the state of Denmark...";
   } while (unDone.length > 0);
 
+  /* --------------------------------------------------------*/
+  console.log("\n makeTree result===", JSON.stringify(result, 0, 2));
+  /* --------------------------------------------------------*
+   **/
   return result;
 
   function setResult(arrayIn, nb_rej) {
